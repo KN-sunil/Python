09de2dcdbee38cdf419ac8cdbe928f19ddcc84b3
KN-sunil <sunilsun837@gmail.com>
2024-04-29 10:53:31 +0530

    .

---
+++ python basics/func(Decorators)/two.py
@@ -0,0 +1,11 @@
+class Test:
+    def __init__(self):
+        pass
+    def deposit(self,amount):
+        pass
+    @classmethod               #class method and static method are builtin decortors
+    def get_min_bal(cls):
+        pass 
+    @staticmethod
+    def calc_int(amount,rate):
+        pass 
