a26aee17b4fa91be042ffcda72c75dfa627e593c
KN-sunil <sunilsun837@gmail.com>
2024-04-30 11:46:46 +0530

    .

---
+++ python basics/func(Variables)/two.py
@@ -0,0 +1,18 @@
+a=10
+b=20
+
+# def f1():
+#     c=30
+#     d=40
+#     print(c)
+#     print(d)
+
+# f1()
+# print(b)
+# print(c) name error because c is local scope
+
+def f2():
+    print(a)
+    print(c)
+f2()    
+   
