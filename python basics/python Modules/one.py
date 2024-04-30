6f915e7c5547751b8d91da54c6827afd9c2c51d5
KN-sunil <sunilsun837@gmail.com>
2024-04-30 12:13:16 +0530

    .

---
+++ python basics/python Modules/one.py
@@ -0,0 +1,15 @@
+'''
+Module=every python file acting as modules 
+module conatins variable statements,function statements,class statements, the statements are called members
+
+if we want access members means we should use "import" module
+*to know about your module  use "help(),dir() modules"
+'''
+
+a=10
+b=20
+c=30
+
+# def wish():
+#     print("good morning")
+# wish()    
---
+++ python basics/python Modules/two.py
@@ -0,0 +1,13 @@
+import one
+from one import *
+print(a)
+print(b)
+
+
+"""
+many ways to import
+-import one
+-from one import abc
+-from one import * this will import all members in module
+-from one import a
+"""
