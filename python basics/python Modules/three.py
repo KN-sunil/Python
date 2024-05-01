0190986a8fba3ee5809149d7ca9afd84e069ae92
KN-sunil <sunilsun837@gmail.com>
2024-05-01 12:19:51 +0530

    .

---
+++ python basics/python Modules/three.py
@@ -0,0 +1,11 @@
+'''
+math() module:
+this modules allows you to perform mathematical tasks or number
+
+some math()module functions:
+'''
+
+x=min(5,10,25)
+y=max(9,30,50)
+print(x)
+print(y)
--- python basics/python Modules/two.py
+++ python basics/python Modules/two.py
@@ -10,4 +10,6 @@ many ways to import
 -from one import abc
 -from one import * this will import all members in module
 -from one import a
+-from modulename import pi as sunil   #member aliasing 
+-from modulename import module1 as one module2 as two module3 as three
 """
