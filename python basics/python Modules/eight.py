afa81a9d7c1fe69177a82c704a7eca2bc77adc27
KN-sunil <sunilsun837@gmail.com>
2024-05-01 12:45:21 +0530

    .

---
+++ python basics/python Modules/eight.py
@@ -0,0 +1,14 @@
+#randrage():returns the random number from the range
+
+# from random import randrange
+# for x in range(15):
+#    print(randrange(50,150,10))
+
+
+
+
+
+
+from random import randrange
+for x in range(10):
+   print(randrange(50,150))   
