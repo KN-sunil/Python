2a3aaba28238e2218f7654798375a72055f0f042
KN-sunil <sunilsun837@gmail.com>
2024-05-01 12:35:45 +0530

    .

---
+++ python basics/python Modules/six.py
@@ -0,0 +1,19 @@
+'''
+random()module-contains several functions to generate random values
+some random functions are:
+randint()
+randrange()
+random()
+choice
+uniform()
+'''
+
+# from random import randint
+# for x in range(10):
+#     print(randint(10,100))
+
+
+
+from random import randint
+for x in range(10):
+    print(randint(99,200))   
