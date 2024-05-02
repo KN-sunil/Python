924b982510db512d559dd552e9d009d76c14a520
KN-sunil <sunilsun837@gmail.com>
2024-05-02 11:37:10 +0530

    .

---
+++ python basics/File Handling/JSON/one.py
@@ -0,0 +1,11 @@
+'''
+how to handle json data:we should import inbuilt json module
+
+json module members:
+-load,dump(works with file)
+-loads,dumps(works without file)
+'''
+
+import json
+
+print(dir(json))
