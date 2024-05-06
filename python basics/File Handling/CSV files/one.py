5603d2b18833d26198f9b7f0c85a69455d351053
KN-sunil <sunilsun837@gmail.com>
2024-05-06 11:58:51 +0530

    .

---
+++ python basics/File Handling/CSV files/one.py
@@ -0,0 +1,15 @@
+# CSV FILE:defines data in the form of comma separate version
+# how to handle:using inbuilt csv module
+
+# csv module members:
+# -writer()
+# -reader()
+# -writers()
+
+#read csv file and print data
+import csv
+fp=open("data.csv","r")
+user_data=csv.reader(fp)
+users=list(user_data)
+print(users)
+
