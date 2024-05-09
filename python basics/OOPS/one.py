f11333a1c1bcfc258860a18b84d07aa12fcce006
KN-sunil <sunilsun837@gmail.com>
2024-05-09 10:58:04 +0530

    .

---
+++ python basics/OOPS/one.py
@@ -0,0 +1,16 @@
+class Account:
+    def open_acc(self):                      #self is pointer pointing to current object used to acces the members inside class
+                                                  #for outside of the class we object
+        print("account opened")
+    def deposit_amount(self):
+        print("amount deposited")
+    def withdrawl(self):
+        print("amount withdrawl")
+    def get_bal(self):
+        print('balance')
+
+a1=Account()
+a1.open_acc()
+a1.deposit_amount()
+a1.withdrawl()
+a1.get_bal()
