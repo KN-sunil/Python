f26903a0f984035b5e0e7483845327b7b5388fc2
KN-sunil <sunilsun837@gmail.com>
2024-05-09 11:26:48 +0530

    .

---
+++ python basics/OOPS/two.py
@@ -0,0 +1,19 @@
+"""
+VARIABLES:
+-INSTANCE VARIABLES:
+Variable value is varied from object to object
+
+-STATIC VARIABLE:
+Variable value is not varied from object to object
+
+-LOCAL VARIABLE:
+Temporary variable local scope
+
+                  org_name="tcs"(static varible creates once at class level and shared with all objects)
+
+eid=101              eid=102               eid=103                
+ename="rahul"       ename="sonia"          ename="priya" 
+esal=45000          esal=50000             esal=55000
+
+here org_name is static variable thats way we are keeping that at class level
+"""
