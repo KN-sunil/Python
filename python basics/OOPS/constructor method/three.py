e78d022f3c268f9b68044f8f38ec441b5207646f
KN-sunil <sunilsun837@gmail.com>
2024-05-10 11:14:58 +0530

    .

---
+++ python basics/OOPS/constructor method/three.py
@@ -0,0 +1,10 @@
+class Account:
+    def open_account(self):
+        print("account opened successfully")
+
+    def deposit_amount(self,amount):
+        print("amount deposited successfully")
+
+a1=Account()
+a1.open_account()  
+a1.deposit_amount(5000)   
