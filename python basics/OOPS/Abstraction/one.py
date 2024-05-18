353d68ad3056141eccf36896c4d45f1403754f46
KN-sunil <sunilsun837@gmail.com>
2024-05-18 15:18:40 +0530

    .

---
+++ python basics/OOPS/Abstraction/one.py
@@ -0,0 +1,22 @@
+# ABSTRACTION:
+# Is nothing but hiding the details an implementation of code
+
+
+class Account:
+    def __init__(self):
+        pass
+
+    def m1(self):
+        pass
+
+    @classmethod
+    def m2(cls):
+        pass
+
+    @staticmethod
+    def m3():
+        pass
+
+    @abstractmethod
+    def m4(self):
+        pass
