d195d18a2eeaf885124dd3c2cf8a059ed52a70e5
KN-sunil <sunilsun837@gmail.com>
2024-05-18 11:51:11 +0530

    .

---
+++ python basics/OOPS/class methods/one.py
@@ -0,0 +1,21 @@
+"""
+Class methods:
+1.constructor method(speical method used to initialize object values)
+2.instance method
+3.class method
+4.static method
+"""
+
+class Test:
+    def __init__(self):
+        pass
+    def m1(self):
+        pass
+
+    @classmethod
+    def m2(cls):
+        pass
+
+    @staticmethod
+    def m3():
+        pass
